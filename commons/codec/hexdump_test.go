a401b64b5c2227f30b3743436fe0862436fc8ac0
fanhaodong.516 <fanhaodong.516@bytedance.com>
2022-08-11 14:47:54 +0800

    fix: tcpdump unused code

--- commons/codec/hexdump_test.go
+++ commons/codec/hexdump_test.go
@@ -1,20 +1,26 @@
 package codec
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestReadHexdump(t *testing.T) {
 	t.Run("data", func(t *testing.T) {
-		data:=`00:02:30.058133 IP6 localhost.36962 > localhost.smc-https: Flags [P.], seq 1:84, ack 1, win 43, options [nop,nop,TS val 3942430538 ecr 3942430538], length 83`
+		data := `00:02:30.058133 IP6 localhost.36962 > localhost.smc-https: Flags [P.], seq 1:84, ack 1, win 43, options [nop,nop,TS val 3942430538 ecr 3942430538], length 83`
 		hexdump, b := ReadHexdump(data)
-		t.Log(hexdump,b)
+		assert.Equal(t, hexdump, "")
+		assert.Equal(t, b, false)
 	})
 	t.Run("payload", func(t *testing.T) {
-		data:=`	0x0040:  eafc b74a eafc b74a 4745 5420 2f68 656c  ...J...JGET./hel`
+		data := `	0x0040:  eafc b74a eafc b74a 4745 5420 2f68 656c  ...J...JGET./hel`
 		hexdump, b := ReadHexdump(data)
-		t.Log(hexdump,b)
+		assert.Equal(t, hexdump, "eafcb74aeafcb74a474554202f68656c")
+		assert.Equal(t, b, false)
 	})
 	t.Run("test", func(t *testing.T) {
-		t.Log(DefaultHexDumpConfig.HexPrefixRegexp.MatchString(`0x0011`))
-		t.Log(DefaultHexDumpConfig.HexPrefixRegexp.MatchString(`0x02`))
+		assert.Equal(t, DefaultHexDumpConfig.HexPrefixRegexp.MatchString(`0x0011`), true)
+		assert.Equal(t, DefaultHexDumpConfig.HexPrefixRegexp.MatchString(`0x02`), true)
 	})
 }
